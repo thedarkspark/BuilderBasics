894b59ea30e3f71a303e736d9de7ef9b1f29f217
thedarkspark <Dwaynebweaver@gmail.com>
2017-07-22 23:20:32 -0500

    This is a basic push button app. My first creation

--- interfaceBuilderApp/ViewController.swift
+++ interfaceBuilderApp/ViewController.swift
@@ -9,8 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var myButton: UIButton!
+    @IBAction func changeTitle(_ sender: Any){
+    mainLabel.text = "Damn this a good app"
+        print("the button was pressed, GOOD JOB!!)")
+    }
+    @IBOutlet weak var mainLabel: UILabel!
 
     override func viewDidLoad() {
+        
+        myButton.setTitleColor(.red, for: .normal)
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
